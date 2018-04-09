b2a275022ee462a7ce4359b71729da77e292e7aa
HusamHusseinHasan <38226885+HusamHusseinHasan@users.noreply.github.com>
2018-04-09 22:38:38 +0300

    Add files via upload

---
+++ gpsworkingmega.ino
@@ -0,0 +1,111 @@
+int updates;
+int failedUpdates;
+int pos;
+int stringplace = 0;
+
+String timeUp;
+String nmea[12];
+String labels[9] {"Time: ", "Status: ", "Latitude: ", "Hemisphere: ", "Longitude: ", "Hemisphere: ", "Speed: ", "Track Angle: ", "Date: "};
+void setup() {
+  Serial.begin(57600);
+  Serial1.begin(9600);
+}
+
+void loop() {
+  Serial.flush();
+  Serial1.flush();
+  while (Serial1.available() > 0)
+  {
+    Serial1.read();
+     //Serial.print(Serial3.read());
+  }
+  if (Serial1.find("$GPRMC,")) {
+    String tempMsg = Serial1.readStringUntil('\n');
+    for (int i = 0; i < tempMsg.length(); i++) {
+      if (tempMsg.substring(i, i + 1) == ",") {
+        nmea[pos] = tempMsg.substring(stringplace, i);
+        stringplace = i + 1;
+        pos++;
+      }
+      if (i == tempMsg.length() - 1) {
+        nmea[pos] = tempMsg.substring(stringplace, i);
+      }
+    }
+    updates++;
+    nmea[2] = ConvertLat();
+    nmea[4] = ConvertLng();
+    for (int i = 0; i < 9; i++) {
+      Serial.print(labels[i]);
+      Serial.print(nmea[i]);
+      Serial.println("");
+    }
+
+  }
+  else {
+
+    failedUpdates++;
+
+  }
+  stringplace = 0;
+  pos = 0;
+}
+
+String ConvertLat() {
+  String posneg = "";
+  if (nmea[3] == "S") {
+    posneg = "-";
+  }
+  String latfirst;
+  float latsecond;
+  for (int i = 0; i < nmea[2].length(); i++) {
+    if (nmea[2].substring(i, i + 1) == ".") {
+      latfirst = nmea[2].substring(0, i - 2);
+      latsecond = nmea[2].substring(i - 2).toFloat();
+    }
+  }
+  latsecond = latsecond / 60;
+  String CalcLat = "";
+
+  char charVal[9];
+  dtostrf(latsecond, 4, 6, charVal);
+  for (int i = 0; i < sizeof(charVal); i++)
+  {
+    CalcLat += charVal[i];
+  }
+  latfirst += CalcLat.substring(1);
+  latfirst = posneg += latfirst;
+  return latfirst;
+}
+
+String ConvertLng() {
+  String posneg = "";
+  if (nmea[5] == "W") {
+    posneg = "-";
+  }
+
+  String lngfirst;
+  float lngsecond;
+  for (int i = 0; i < nmea[4].length(); i++) {
+    if (nmea[4].substring(i, i + 1) == ".") {
+      lngfirst = nmea[4].substring(0, i - 2);
+      //Serial.println(lngfirst);
+      lngsecond = nmea[4].substring(i - 2).toFloat();
+      //Serial.println(lngsecond);
+
+    }
+  }
+  lngsecond = lngsecond / 60;
+  String CalcLng = "";
+  char charVal[9];
+  dtostrf(lngsecond, 4, 6, charVal);
+  for (int i = 0; i < sizeof(charVal); i++)
+  {
+    CalcLng += charVal[i];
+  }
+  lngfirst += CalcLng.substring(1);
+  lngfirst = posneg += lngfirst;
+  return lngfirst;
+}
+
+
+
